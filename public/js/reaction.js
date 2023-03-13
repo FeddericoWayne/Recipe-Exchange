926a7f983e0c67e83f3546fe136293d665ad2f04
FeddericoWayne <jackiew1120@hotmail.com>
2023-03-13 13:18:07 -0400

    added copy recipe button and function

--- public/js/reaction.js
+++ public/js/reaction.js
@@ -4,6 +4,10 @@ const likeButton = document.getElementById("like");
 const dislikeButton = document.getElementById("dislike");
 const recipeId = document.getElementById("commentText");
 const user = document.getElementById("like");
+const copyButton = document.getElementById("copy");
+const copyWrapper = document.getElementById("copy-wrapper");
+const copyAlert = document.getElementById("copy-alert");
+const recipeText = document.getElementById("recipe-text");
 
 
 async function updateLikes() {
@@ -46,12 +50,39 @@ async function updateDislikes() {
         return;
     };
 
-}
+};
+
+function showTip(e) {
+
+    if (e.target.matches("img")) {
+        copyAlert.removeAttribute("style","visibility:hidden");
+    } else {
+        copyAlert.setAttribute("style","visibility:hidden");
+    }
+
+};
+
+function copyRecipe() {
+    // changes alert text
+    copyAlert.innerHTML = "Recipe Copied!";
+
+    // changes alert text back to default and sets timeout for alert to disappear in 1 second
+    setTimeout(() =>{
+        copyAlert.setAttribute("style","visibility:hidden");
+        copyAlert.innerHTML = "Copy Recipe?";
+    },1000);
 
 
+    // copies to browser clipboard
+    navigator.clipboard.writeText(recipeText.value);
+
+};
 
 
 
 // event listenters
 likeButton.addEventListener("click",updateLikes);
 dislikeButton.addEventListener("click",updateDislikes);
+copyWrapper.addEventListener("mouseover",showTip);
+copyButton.addEventListener("click",copyRecipe);
+
